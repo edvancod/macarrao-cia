86dbdb24a40100d744b4575fbacc7759b3c30812
Edvan Silva <edvan.s.pereira@aluno.senai.br>
2025-02-22 17:01:57 -0300

    inicial commite

---
+++ script/main.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("sendBtn").addEventListener("click", function () {
+        let telefone = "5561985945661"; 
+        let mensagem = "*Pedido - Macarrão e Cia*%0A%0A";
+
+        let tamanho = document.querySelector("input[name='tamanho']:checked")?.value || "Não informado";
+        let massa = document.querySelector("input[name='massa']:checked")?.value || "Não informado";
+        let molho = document.querySelector("input[name='molho']:checked")?.value || "Não informado";
+        let carne = document.querySelector("input[name='carne']:checked")?.value || "Não informado";
+
+        let acompanhamentos = Array.from(document.querySelectorAll("input[name='acompanhamento']:checked"))
+            .map(item => item.value)
+            .join(", ") || "Nenhum";
+
+        let url = `https://wa.me/${telefone}?text=${mensagem}🍝 *Massa:* ${massa}%0A🥫 *Molho:* ${molho}%0A🍖 *Carne:* ${carne}%0A🍽️ *Acompanhamentos:* ${acompanhamentos}`;
+        window.open(url, "_blank");
+    });
+});
